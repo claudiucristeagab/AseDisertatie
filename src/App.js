cf7d5c587af5875569c0e561e044b6128579f173
Cristea Claudiu Gabriel <cldcrst96@gmail.com>
2020-03-21 18:10:55 +0200

    Created the Header. Added scripts.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
+import { Header } from './shared/Header'
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './shared/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        APP
       </div>
     );
   }
