a43f4ad906856f5dd11d9a96a9960fe685c63e25
Cristea Claudiu Gabriel <cldcrst96@gmail.com>
2020-03-22 00:19:35 +0200

    changed to paths from 'src'. Moved components around in new folders.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { init } from './reducers';
+import { init } from 'reducers';
 
-import { Header } from './shared/Header';
-import RentalDetail from './components/rental/RentalDetail';
-import RentalList from './components/rental/RentalList';
-import './App.css';
+import { Header } from 'shared/Header';
+import RentalDetail from 'components/rental/rentalDetail/RentalDetail';
+import RentalList from 'components/rental/rentalListing/RentalList';
+import 'App.css';
 
 class App extends Component {
   render() {
